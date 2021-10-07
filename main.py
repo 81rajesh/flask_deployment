771080a456a2343f5df69f86d628db3b51935921
Rajesh Patil <rajeshpatil271097@gmail.com>
2021-10-07 09:32:57 +0530

    first commit

--- main.py
+++ main.py
@@ -4,23 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-from flask import Flask, render_template,request,jsonify
+from flask import Flask,request,render_template,jsonify
 
-app= Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return '<h1> This is the Flask Application </h1>'
+    return '<h1> This is the flask application </h1>'
 
 if __name__ == '__main__':
     app.run()
